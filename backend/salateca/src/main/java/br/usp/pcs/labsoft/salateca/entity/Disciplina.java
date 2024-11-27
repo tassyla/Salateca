1bc01d6a598f0a9c9b3697acee45c327ba94d8bb
Buenobarbie <barbarabueno140104@gmail.com>
2024-11-27 01:48:07 -0300

    ✨ Disciplina é quem cria turmas

--- backend/salateca/src/main/java/br/usp/pcs/labsoft/salateca/entity/Disciplina.java
+++ backend/salateca/src/main/java/br/usp/pcs/labsoft/salateca/entity/Disciplina.java
@@ -1,7 +1,10 @@
 package br.usp.pcs.labsoft.salateca.entity;
 
 import jakarta.persistence.*;
+
+import java.time.LocalTime;
 import java.util.Collection;
+import java.util.List;
 
 @Entity
 public class Disciplina {
@@ -17,7 +20,7 @@ public class Disciplina {
     // Quando uma disciplina é excluída, as turmas
     // associadas a ela também são excluídas
     @OneToMany(mappedBy = "disciplina", cascade = CascadeType.ALL)
-    private Collection<Turma> turmas; 
+    private Collection<Turma> turmas;
 
     // Construtores
     public Disciplina() {}
@@ -56,7 +59,28 @@ public class Disciplina {
         return turmas;
     }
 
-    public void setSalas(Collection<Turma> turmas) {
+    public void setTurmas(Collection<Turma> turmas) {
         this.turmas = turmas;
+    }
+
+    // Criar turma e associá-la à disciplina
+    public Turma criarTurma(String codigo, String nome, boolean requerComputador, List<String> diasDaSemana, 
+                            List<LocalTime> horariosInicios, List<LocalTime> horariosFins, 
+                            Integer quantidadeComputadores, String sistemaOperacional) {
+
+        Turma novaTurma = new Turma(codigo, nome, this, quantidadeComputadores, sistemaOperacional,
+                                    diasDaSemana, horariosInicios, horariosFins);
+
+        
+                                    
+        this.turmas.add(novaTurma);
+
+        return novaTurma;
+    }
+
+
+    // Método para listar todas as turmas
+    public Collection<Turma> listarTurmas() {
+        return turmas;
     }
 }
